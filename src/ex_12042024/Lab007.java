2cb3f7ff717b15faa0853cd8d482462fbb03944f
Ratul-Tester <ratul.tester@gmail.com>
2024-05-21 23:41:48 +0530

    12 April 2024

---
+++ src/ex_12042024/Lab007.java
@@ -0,0 +1,55 @@
+package ex_12042024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        byte minValueByte = Byte.MIN_VALUE;
+        byte maxValueByte = Byte.MAX_VALUE;
+        // Byte is a primitive data type and it holds the below range
+        System.out.println("Byte is a primitive data type and it holds the below range");
+        System.out.println("The range of Byte is from "+minValueByte +" to "+maxValueByte +"\n");
+        //---------------------------------------------------------------------------------------
+        short minValueShort = Short.MIN_VALUE;
+        short maxValueShort = Short.MAX_VALUE;
+        // short is a primitive data type and it holds the below range
+        System.out.println("Short is a primitive data type and it holds the below range");
+        System.out.println("The range of Short is from "+minValueShort +" to "+maxValueShort +"\n");
+        //------------------------------------------------------------------------------------------
+        int minValueInt = Integer.MIN_VALUE;
+        int maxValueInt = Integer.MAX_VALUE;
+        // Int is a primitive data type and it holds the below range
+        System.out.println("Int is a primitive data type and it holds the below range");
+        System.out.println("The range of Int is from "+minValueInt +" to "+maxValueInt +"\n");
+        //------------------------------------------------------------------------------------
+        char minValueChar = Character.MIN_VALUE;
+        char maxValueChar = Character.MAX_VALUE;
+        // Char is a primitive data type and it holds the below range
+        System.out.println("Char is a primitive data type and it holds the below range");
+        System.out.println("The range of Char is from "+minValueChar +" to "+maxValueChar +"\n");
+        //---------------------------------------------------------------------------------------
+        long minValueLong = Long.MIN_VALUE;
+        long maxValueLong = Long.MAX_VALUE;
+        // Long is a primitive data type and it holds the below range
+        System.out.println("Long is a primitive data type and it holds the below range");
+        System.out.println("The range of Long is from "+minValueLong +" to "+maxValueLong +"\n");
+        //---------------------------------------------------------------------------------------
+        float minValueFloat = Float.MIN_VALUE;
+        float maxValueFloat = Float.MAX_VALUE;
+        // Float is a primitive data type and it holds the below range
+        System.out.println("Float is a primitive data type and it holds the below range");
+        System.out.println("The range of Float is from "+minValueFloat +" to "+maxValueFloat +"\n");
+        //------------------------------------------------------------------------------------------
+        double minValueDouble = Double.MIN_VALUE;
+        double maxValueDouble = Double.MAX_VALUE;
+        // Double is a primitive data type and it holds the below range
+        System.out.println("Double is a primitive data type and it holds the below range");
+        System.out.println("The range of Double is from "+minValueDouble +" to "+maxValueDouble +"\n");
+        //---------------------------------------------------------------------------------------------
+        boolean minValueBoolean = Boolean.FALSE;
+        boolean maxValueBoolean = Boolean.TRUE;
+        // Boolean is a primitive data type and it holds the below range
+        System.out.println("Boolean is a primitive data type and it holds the below range");
+        System.out.println("The range of Boolean is from "+minValueBoolean +" to "+maxValueBoolean +"\n");
+
+
+    }
+}
