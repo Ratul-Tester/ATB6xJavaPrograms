3b359a123268eb98f1d9ac9ca96c820fcde937ad
Ratul-Tester <ratul.tester@gmail.com>
2024-05-21 13:05:07 +0530

    11 April 2024

---
+++ src/ex_11042024/Lab001.java
@@ -0,0 +1,15 @@
+package ex_11042024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        // Single line comment - This will not execute.
+        // exit code 0 (Means code is successfully executed)
+        /* Commenting multiple lines
+        kjhkjhkjhjkh
+        kjhkjhkjhkjhkjh
+        jhjkhjkhjkh
+        bla bla bla
+        */
+        System.out.println("Hello World");
+    }
+}
