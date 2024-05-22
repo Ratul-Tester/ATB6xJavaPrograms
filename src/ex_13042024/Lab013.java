d97e951206f49c7f6ce4682ff42f3ea1d5f938d8
Ratul-Tester <ratul.tester@gmail.com>
2024-05-22 21:47:09 +0530

    13 April 2024

---
+++ src/ex_13042024/Lab013.java
@@ -0,0 +1,15 @@
+package ex_13042024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        char c = 'A';
+        char $ = '$';
+        char _1 = '9';
+        char q_ = '/';
+        System.out.println(c);
+        System.out.println($);
+        System.out.println(_1);
+        System.out.println(q_);
+
+    }
+}
