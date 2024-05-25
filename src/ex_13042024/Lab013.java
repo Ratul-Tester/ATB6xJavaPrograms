6664595e53360d979e131a4b5c0d0f5ee4646057
Ratul-Tester <ratul.tester@gmail.com>
2024-05-25 21:21:52 +0530

    13 April 2024

--- src/ex_13042024/Lab013.java
+++ src/ex_13042024/Lab013.java
@@ -6,10 +6,23 @@ public class Lab013 {
         char $ = '$';
         char _1 = '9';
         char q_ = '/';
+        char g = '\u1F00';
+        char h = '\u1F01';
+        char i = '\u1F03';
+        char j = '\u1F04';
+        char k = '\u1F05';
+        char l = '\u1F6F';
         System.out.println(c);
         System.out.println($);
         System.out.println(_1);
         System.out.println(q_);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+
 
     }
 }
