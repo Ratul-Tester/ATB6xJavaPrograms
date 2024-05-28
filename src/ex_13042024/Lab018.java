6f19fba2aedef070a59d6038e4ff6feb01637724
Ratul-Tester <ratul.tester@gmail.com>
2024-05-28 09:36:29 +0530

    13 April 2024

---
+++ src/ex_13042024/Lab018.java
@@ -0,0 +1,9 @@
+package ex_13042024;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        String firstName = "Ratul";
+        String lastName = " Nandy";
+        System.out.println(firstName + lastName); //Concatenation with Strings...
+    }
+}
