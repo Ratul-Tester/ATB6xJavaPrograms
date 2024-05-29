14116a226b794aeebe21b9c4a41f6ac743bd0ee1
Ratul-Tester <ratul.tester@gmail.com>
2024-05-29 23:24:06 +0530

    18 April 2024

---
+++ src/ex_18042024/Lab021.java
@@ -0,0 +1,24 @@
+package ex_18042024;
+
+public class Lab021 {
+    public static void main(String[] args) {
+        //Compound Assignment Operators...
+        //+=, -=, *=, /=, %=
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+        a+=1;
+        b-=1;
+        c*=2;
+        d/=2;
+        e%=2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
---
+++ src/ex_18042024/Lab023.java
@@ -0,0 +1,20 @@
+package ex_18042024;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        System.out.println(10>=10);
+        System.out.println(10>10);
+        System.out.println(10<=10);
+        System.out.println(10<10);
+        System.out.println('A');
+        System.out.println('A'!=65);
+        System.out.println('A'==65);
+        System.out.println('A'==65.0);
+        System.out.println('A'==65.2);
+        //System.out.println(true>=true);----> Boolean cannot be compared
+        System.out.println(true==true);
+        System.out.println(true==false);
+        System.out.println(false==false);
+        System.out.println(false==true);
+    }
+}
